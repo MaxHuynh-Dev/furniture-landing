42cc6fcf173bca9ef29e2f0021624a8768853631
max <max>
2025-12-31 14:37:35 +0700

    Enhance SearchBar component: integrate icon and button for improved user interaction, and add corresponding styles for better layout and functionality.

--- src/layout/Header/Main/SearchBar/index.tsx
+++ src/layout/Header/Main/SearchBar/index.tsx
@@ -1,10 +1,20 @@
+import Text from '@/components/Typo';
+import { Search } from 'lucide-react';
 import type React from 'react';
 import styles from './searchBar.module.scss';
 
 function SearchBar(): React.ReactElement {
   return (
     <div className={styles.searchBar}>
-      <input type="text" placeholder="Search" />
+      <div className={styles.searchBar_icon}>
+        <Search width={'2rem'} height={'2rem'} />
+        <input type="text" placeholder="Search" className={styles.searchBar_input} />
+      </div>
+      <button className={styles.searchBar_button}>
+        <Text size={14} weight="semibold" color="white">
+          Search
+        </Text>
+      </button>
     </div>
   );
 }
